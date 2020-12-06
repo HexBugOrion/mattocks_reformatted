09c2ccab035ff2a841f221ff22afe41868720e16
HexBugOrion <58091259+HexBugOrion@users.noreply.github.com>
2020-12-05 23:54:56 -0600

    Fixed Pendorite and Netherite mattocks

--- src/main/java/net/oriondev/mattocks2/Mattocks2.java
+++ src/main/java/net/oriondev/mattocks2/Mattocks2.java
@@ -7,28 +7,23 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ToolMaterials;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
-import net.oriondev.mattocks2.mattock_type.BotchedToolMaterialsEnderite;
 import net.oriondev.mattocks2.mattock_type.BotchedToolMaterialsPendorite;
 import net.oriondev.mattocks2.mattock_type.Mattock;
 
 public class Mattocks2 implements ModInitializer {
 
 	public static final Mattock PENDORITE_MATTOCK = new Mattock(7, -3, new BotchedToolMaterialsPendorite(), new Item.Settings().group(ItemGroup.TOOLS).fireproof());
-	public static final Mattock ENDERITE_MATTOCK = new Mattock(7, -3, new BotchedToolMaterialsEnderite(), new Item.Settings().group(ItemGroup.TOOLS).fireproof());
-	public static final Mattock NETHERITE_MATTOCK = new Mattock(6, -3, ToolMaterials.NETHERITE, new Item.Settings().group(ItemGroup.TOOLS));
+	public static final Mattock NETHERITE_MATTOCK = new Mattock(6, -3, ToolMaterials.NETHERITE, new Item.Settings().group(ItemGroup.TOOLS).fireproof());
 	public static final Mattock DIAMOND_MATTOCK = new Mattock(3, -3, ToolMaterials.DIAMOND, new Item.Settings().group(ItemGroup.TOOLS));
 	public static final Mattock IRON_MATTOCK = new Mattock(3, -3, ToolMaterials.IRON, new Item.Settings().group(ItemGroup.TOOLS));
-	public static final Mattock STONE_MATTOCK = new Mattock(3, -3, ToolMaterials.STONE, new Item.Settings().group(
-		ItemGroup.TOOLS));
+	public static final Mattock STONE_MATTOCK = new Mattock(3, -3, ToolMaterials.STONE, new Item.Settings().group(ItemGroup.TOOLS));
 	public static final Mattock WOODEN_MATTOCK = new Mattock(2, -3, ToolMaterials.WOOD, new Item.Settings().group(ItemGroup.TOOLS));
 	public static final Mattock GOLDEN_MATTOCK = new Mattock(3, -3, ToolMaterials.GOLD, new Item.Settings().group(ItemGroup.TOOLS));
+
 	@Override
 	public void onInitialize() {
-		if(FabricLoader.INSTANCE.isModLoaded("enderitemod")) {
-			Registry.register(Registry.ITEM, new Identifier("mattocks", "enderite_mattock"), ENDERITE_MATTOCK);
-		}
 		if(FabricLoader.INSTANCE.isModLoaded("byg")) {
-			Registry.register(Registry.ITEM, new Identifier("mattocks", "enderite_mattock"), PENDORITE_MATTOCK);
+			Registry.register(Registry.ITEM, new Identifier("mattocks2", "pendorite_mattock"), PENDORITE_MATTOCK);
 		}
 		Registry.register(Registry.ITEM, new Identifier("mattocks2", "netherite_mattock"), NETHERITE_MATTOCK);
 		Registry.register(Registry.ITEM, new Identifier("mattocks2", "diamond_mattock"), DIAMOND_MATTOCK);
